0eca51c36d45181385c77c5f459b2944d0ccbfbd
losooooo <147423869+KhalisahNajah@users.noreply.github.com>
2024-06-15 14:09:50 +0800

    Music for mainmenu, tutorial page & best score

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -1,33 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
+    private static string[] scenesWithMusic = { "MainMenuScene", "TutorialScene", "BestScoreScene" };
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        if (audioSource == null)
+        {
+            audioSource = FindObjectOfType<AudioManager>().GetComponent<AudioSource>();
+        }
+    }
+
     public void MainMenu()
     {
         Cursor.lockState = CursorLockMode.None;
+        PlayMusicIfNeeded("MainMenuScene");
         SceneManager.LoadScene("MainMenuScene");
         Debug.Log("Main Menu");
     }
+
     public void Play()
     {
+        Cursor.lockState = CursorLockMode.None;
+        StopMusicIfNeeded("PlayScene");
         SceneManager.LoadScene("PlayScene");
         Debug.Log("Play");
     }
+
     public void DisplayTutorial()
     {
         Cursor.lockState = CursorLockMode.None;
+        PlayMusicIfNeeded("TutorialScene");
         SceneManager.LoadScene("TutorialScene");
         Debug.Log("Tutorial");
     }
+
     public void BestScore()
     {
         Cursor.lockState = CursorLockMode.None;
+        PlayMusicIfNeeded("BestScoreScene");
         SceneManager.LoadScene("BestScoreScene");
         Debug.Log("Best Score");
     }
+
     public void QuitGame()
     {
         #if UNITY_EDITOR
@@ -36,5 +56,24 @@ public class ChangeScene : MonoBehaviour
             Application.Quit();
         #endif
         Debug.Log("Quit");
+    }
+
+    private void StopMusicIfNeeded(string sceneName)
+    {
+        if (audioSource != null && !System.Array.Exists(scenesWithMusic, scene => scene == sceneName))
+        {
+            audioSource.Stop();
+        }
+    }
+
+    private void PlayMusicIfNeeded(string sceneName)
+    {
+        if (audioSource != null && System.Array.Exists(scenesWithMusic, scene => scene == sceneName))
+        {
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
     }
 }
